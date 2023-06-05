98352f265a3baf07ff9b416a86f120c07c396a61
Alphonse Paix <alphonse.paix@outlook.com>
2023-06-05 15:19:16 +0200

    Mise à jour

--- main.cpp
+++ main.cpp
@@ -14,20 +14,27 @@
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
 
-namespace args {
-    double mu = 1;
-    double alpha = 2;
-    double bar_n = 0.9;
-    double p = 1.1;
-    double c = 1e-09;
-    double beta = std::log(10);
-    double tmax = 1e+03;
-    std::string filename = "data.csv";
-    std::string dirname = "data";
-}
-
 const int bar_width = 40;
-const double delay = 0.5;    // refresh delay in seconds
+const double delay = 0.5;   // progress bar update delay in s
+
+struct Config {
+    double tmax;
+    double mu;
+    double alpha;
+    double bar_n;
+    double p;
+    double c;
+    double beta;
+
+    bool generate_seqs;
+    int num_seqs;
+    int max_len;
+
+    std::string filename;
+    std::string dirname;
+
+    bool verbose;
+};
 
 void update_bar(const double progress, const std::string& status = "") {
     const int pos = static_cast<int>(progress * bar_width);
@@ -86,28 +93,29 @@ struct Point {
 
 using Sequence = std::vector<Point>;
 
-Sequence etas(const bool verbose = true) {
+Sequence etas(const Config& cfg) {
     Sequence seq;
     double tc = 0;
     double m_max = 0;
+    bool verbose = cfg.verbose && !cfg.generate_seqs;
 
     if (verbose)
         std::cout << "Generating background earthquakes...\n";
 
-    while (tc < args::tmax) {
-        const double dt = -1 / args::mu * rd::logrand();
+    while (tc < cfg.tmax) {
+        const double dt = -1 / cfg.mu * rd::logrand();
 
         tc += dt;
-        if (tc < args::tmax) {
-            const double m = rd::mag(args::beta);
+        if (tc < cfg.tmax) {
+            const double m = rd::mag(cfg.beta);
 
             m_max = std::max(m_max, m);
             seq.push_back({ tc, m, 0 });
         }
     }
 
-    const double a = args::bar_n * (args::p - 1) * (args::beta - args::alpha)
-        / (args::beta * std::pow(args::c, 1 - args::p));
+    const double a = cfg.bar_n * (cfg.p - 1) * (cfg.beta - cfg.alpha)
+        / (cfg.beta * std::pow(cfg.c, 1 - cfg.p));
 
     if (a > 0) {
         std::size_t nc = 0;
@@ -115,7 +123,7 @@ Sequence etas(const bool verbose = true) {
 
         if (verbose) {
             std::cout << "Generating aftershocks...\n";
-            update_bar(seq[nc].t / args::tmax, to_string(m_max));
+            update_bar(seq[nc].t / cfg.tmax, to_string(m_max));
         }
 
         while (true) {
@@ -126,23 +134,23 @@ Sequence etas(const bool verbose = true) {
                 const std::chrono::duration<double> diff = end - start;
 
                 if (diff.count() > delay) {
-                    update_bar(seq[nc].t / args::tmax, to_string(m_max));
+                    update_bar(seq[nc].t / cfg.tmax, to_string(m_max));
                     start = end;
                 }
             }
 
             while (true) {
-                const double tmp = std::pow(tc + args::c, 1 - args::p) + (args::p - 1)
-                    / (a * std::exp(args::alpha * seq[nc].m))
+                const double tmp = std::pow(tc + cfg.c, 1 - cfg.p) + (cfg.p - 1)
+                    / (a * std::exp(cfg.alpha * seq[nc].m))
                     * rd::logrand();
 
                 if (tmp > 0) {
-                    const double dt = std::pow(tmp, 1 / (1 - args::p)) - tc - args::c;
+                    const double dt = std::pow(tmp, 1 / (1 - cfg.p)) - tc - cfg.c;
                     tc += dt;
                     const double tc_nc = tc + seq[nc].t;
 
-                    if (tc_nc < args::tmax) {
-                        const double m = rd::mag(args::beta);
+                    if (tc_nc < cfg.tmax) {
+                        const double m = rd::mag(cfg.beta);
 
                         m_max = std::max(m_max, m);
                         seq.push_back({ tc_nc, m, nc + 1 });
@@ -166,6 +174,9 @@ Sequence etas(const bool verbose = true) {
             ++nc;
             if (nc >= seq.size())
                 break;
+
+            if (seq.size() > cfg.max_len && cfg.generate_seqs)
+                return seq;     // early return, will not be saved
         }
     }
 
@@ -190,22 +201,21 @@ void write_to_file(const Sequence& seq, const std::string& filename,
         << filename << "`.\n";
 }
 
-void generate_seqs(int num_seqs, std::size_t max_len,
-    const std::string& dirname = "data", bool verbose = true) {
-    if (!std::filesystem::create_directory(dirname)) {
+void generate_seqs(const Config& cfg) {
+    if (!std::filesystem::create_directory(cfg.dirname)) {
         std::cout << "Could not create directory, exiting...\n";
         return;
     }
 
-    if (verbose)
+    if (cfg.verbose)
         std::cout << "Generating sequences...\n";
 
-    for (int i = 1; i <= num_seqs; ++i) {
+    for (int i = 1; i <= cfg.num_seqs; ++i) {
         do {
-            const Sequence seq = etas(false);
+            const Sequence seq = etas(cfg);
 
-            if (seq.size() <= max_len) {
-                const std::string filename = dirname + "/data"
+            if (seq.size() <= cfg.max_len) {
+                const std::string filename = cfg.dirname + '/' + cfg.filename
                     + std::to_string(i) + ".csv";
 
                 write_to_file(seq, filename, false);
@@ -213,106 +223,94 @@ void generate_seqs(int num_seqs, std::size_t max_len,
             }
         } while (true);
 
-        if (verbose)
-            update_bar(static_cast<double>(i) / num_seqs);
+        if (cfg.verbose)
+            update_bar(static_cast<double>(i) / cfg.num_seqs);
     }
 
-    if (verbose)
+    if (cfg.verbose)
         close_bar();
 }
 
-void print_args() {
-    std::cout << "Executing program with following parameters:\n";
-    std::cout << "tmax\t\t" << args::tmax << '\n';
-    std::cout << "mu\t\t" << args::mu << '\n';
-    std::cout << "alpha\t\t" << args::alpha << '\n';
-    std::cout << "bar_n\t\t" << args::bar_n << '\n';
-    std::cout << "p\t\t" << args::p << '\n';
-    std::cout << "c\t\t" << args::c << '\n';
-    std::cout << "beta\t\t" << args::beta << '\n';
-    std::cout << "filename\t" << args::filename << '\n';
-    std::cout << "dirname\t\t" << args::dirname << '\n';
-}
-
-int main(int argc, char* argv[]) {
-    // arguments parsing
-    // -----------------
+Config parse_arguments(int argc, char* argv[]) {
     po::options_description desc("Allowed options");
     desc.add_options()
-        ("help", "ETAS algorithm implementation")
-        ("tmax", po::value<double>(), "")
-        ("mu", po::value<double>(), "")
-        ("alpha", po::value<double>(), "")
-        ("bar_n", po::value<double>(), "")
-        ("p", po::value<double>(), "")
-        ("c", po::value<double>(), "")
-        ("beta", po::value<double>(), "")
-        ("filename", po::value<std::string>(), "")
-        ("dirname", po::value<std::string>(), "")
+        ("tmax", po::value<double>()->default_value(100), "")
+        ("mu", po::value<double>()->default_value(1), "")
+        ("alpha", po::value<double>()->default_value(2), "")
+        ("bar_n", po::value<double>()->default_value(0.9), "")
+        ("p", po::value<double>()->default_value(1.1), "")
+        ("c", po::value<double>()->default_value(1e-09), "")
+        ("beta", po::value<double>()->default_value(std::log(10)), "")
+
+        ("generate_seqs", po::bool_switch()->default_value(false), "")
+        ("num_seqs", po::value<int>()->default_value(100), "")
+        ("max_len", po::value<int>()->default_value(300), "")
+
+        ("filename", po::value<std::string>()->default_value("data.csv"), "")
+        ("dirname", po::value<std::string>()->default_value("data"), "")
+
+        ("verbose", po::bool_switch()->default_value(false), "")
         ;
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 
-    if (vm.count("help")) {
-        std::cout << desc << "\n";
-        return 0;
+    return Config{
+        vm["tmax"].as<double>(),
+        vm["mu"].as<double>(),
+        vm["alpha"].as<double>(),
+        vm["bar_n"].as<double>(),
+        vm["p"].as<double>(),
+        vm["c"].as<double>(),
+        vm["beta"].as<double>(),
+
+        vm["generate_seqs"].as<bool>(),
+        vm["num_seqs"].as<int>(),
+        vm["max_len"].as<int>(),
+
+        vm["filename"].as<std::string>(),
+        vm["dirname"].as<std::string>(),
+
+        vm["verbose"].as<bool>()
+    };
+}
+
+void print_args(const Config& cfg) {
+    std::cout << "Executing program with following parameters:\n";
+    std::cout << std::boolalpha;
+
+    std::cout << "tmax\t\t" << cfg.tmax << '\n';
+    std::cout << "mu\t\t" << cfg.mu << '\n';
+    std::cout << "alpha\t\t" << cfg.alpha << '\n';
+    std::cout << "bar_n\t\t" << cfg.bar_n << '\n';
+    std::cout << "p\t\t" << cfg.p << '\n';
+    std::cout << "c\t\t" << cfg.c << '\n';
+    std::cout << "beta\t\t" << cfg.beta << "\n\n";
+
+    std::cout << "generate_seqs\t" << cfg.generate_seqs << '\n';
+    std::cout << "num_seqs\t" << cfg.num_seqs << '\n';
+    std::cout << "max_len\t\t" << cfg.max_len << "\n\n";
+
+    std::cout << "filename\t" << cfg.filename << '\n';
+    std::cout << "dirname\t\t" << cfg.dirname << "\n\n";
+
+    std::cout << "verbose\t\t" << cfg.verbose << '\n';
+}
+
+int main(int argc, char* argv[]) {
+    const Config cfg = parse_arguments(argc, argv);
+
+    if (cfg.verbose)
+        print_args(cfg);
+    
+    if (cfg.generate_seqs) {
+        generate_seqs(cfg);
     }
-
-    if (vm.count("tmax")) {
-        std::cout << "Setting tmax...\n";
-        args::tmax = vm["tmax"].as<double>();
+    else {
+        auto seq = etas(cfg);
+        write_to_file(seq, cfg.filename, cfg.verbose);
     }
-
-    if (vm.count("mu")) {
-        std::cout << "Setting mu...\n";
-        args::mu = vm["mu"].as<double>();
-    }
-
-    if (vm.count("alpha")) {
-        std::cout << "Setting alpha...\n";
-        args::alpha = vm["alpha"].as<double>();
-    }
-
-    if (vm.count("bar_n")) {
-        std::cout << "Setting bar_n...\n";
-        args::bar_n = vm["bar_n"].as<double>();
-    }
-
-    if (vm.count("p")) {
-        std::cout << "Setting p...\n";
-        args::p = vm["p"].as<double>();
-    }
-
-    if (vm.count("c")) {
-        std::cout << "Setting c...\n";
-        args::c = vm["c"].as<double>();
-    }
-
-    if (vm.count("beta")) {
-        std::cout << "Setting beta...\n";
-        args::beta = vm["beta"].as<double>();
-    }
-
-    if (vm.count("filename")) {
-        std::cout << "Setting filename...\n";
-        args::filename = vm["filename"].as<std::string>();
-    }
-
-    if (vm.count("dirname")) {
-        std::cout << "Setting dirname...\n";
-        args::dirname = vm["dirname"].as<std::string>();
-    }
-
-    // print ETAS simulation arguments
-    // -------------------------------
-    print_args();
-
-    // simulation
-    // ----------    
-    const auto tmax3k = etas();
-    write_to_file(tmax3k, args::filename);
 
     return 0;
 }
