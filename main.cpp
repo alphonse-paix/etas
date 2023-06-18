8c8360a55bcb2e037722f40228ca0ba5694e2df6
Alphonse Paix <alphonse.paix@outlook.com>
2023-06-18 22:29:35 +0200

    Indicators et refactorisation

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+// main.cpp
+// ETAS algorithm
+// argpare, indicators (github.com/p-ranav)
+
 #include <algorithm>
 #include <chrono>
 #include <cmath>
@@ -14,16 +18,31 @@
 #include <thread>
 #include <vector>
 
-#include "argparse.hpp"
+#include <argparse/argparse.hpp>
+#include <indicators/progress_bar.hpp>
+#include <indicators/cursor_control.hpp>
 
-namespace pbar_settings
-{
-    constexpr int bar_width     = 40;
-    constexpr double delay      = 0.5;
-    constexpr char fill         = '=';
-    constexpr char lead         = '>';
-    constexpr char remainder    = ' ';
-}
+// forward declarations
+// ------------------------------------------------------------------------------------------------
+
+struct Config;
+Config parse_arguments(int argc, char* argv[]);
+void print_args(const Config& cfg);
+
+/*
+namespace rd
+    double logrand()
+    dobule mag(double beta)
+*/
+
+struct Event;
+using Sequence = std::vector<Event>;
+Sequence etas(const Config& cfg);
+void generate_seqs(const Config& cfg);
+void write_to_file(const Sequence& seq, const std::string& filename, bool verbose);
+
+// program configuration related functions
+// ------------------------------------------------------------------------------------------------
 
 struct Config
 {
@@ -45,229 +64,8 @@ struct Config
     bool verbose;
 };
 
-template<typename T>
-void update_bar(const double progress, const T& status)
-{
-    const int pos = static_cast<int>(progress * pbar_settings::bar_width);
-
-    std::cout << '[';
-    for (int i = 0; i < pbar_settings::bar_width; ++i) {
-        if (i < pos)        std::cout << pbar_settings::fill;
-        else if (i == pos)  std::cout << pbar_settings::lead;
-        else                std::cout << pbar_settings::remainder;
-    }
-    std::cout << "] " << std::setw(3) << static_cast<int>(progress * 100)
-                      << "% " << status;
-    std::cout << '\r';
-    std::cout.flush();
-}
-
-template<typename T>
-void close_bar(const T& status)
-{
-    update_bar(1, status);
-    std::cout << '\n';
-}
-
-namespace rd
-{
-    inline std::mt19937 init()
-    {
-        std::random_device rd;
-        std::seed_seq ss{ rd(), rd(), rd(), rd() };
-
-        return std::mt19937{ ss };
-    }
-
-    inline std::mt19937 mt = init();
-    inline std::uniform_real_distribution<double> d{ 0, 1 };
-
-    inline double logrand()
-    {
-        return std::log(d(mt));
-    }
-
-    inline double mag(double beta)
-    {
-        return -1 / beta * logrand();
-    }
-}
-
-struct Point
-{
-    double          t;
-    double          m;
-    std::size_t     parent;
-};
-
-using Sequence = std::vector<Point>;
-
-Sequence etas(const Config& cfg)
-{
-    Sequence seq;
-    double tc = 0;
-    double m_max = 0;
-    bool verbose = cfg.verbose && !cfg.generate_seqs;
-
-    if (verbose)
-        std::cout << "Generating background earthquakes...\n";
-
-    while (tc < cfg.tmax) {
-        const double dt = -1 / cfg.mu * rd::logrand();
-
-        tc += dt;
-        if (tc < cfg.tmax) {
-            const double m = rd::mag(cfg.beta);
-
-            m_max = std::max(m_max, m);
-            seq.push_back({ tc, m, 0 });
-        }
-    }
-
-    const double a = cfg.bar_n * (cfg.p - 1) * (cfg.beta - cfg.alpha)
-        / (cfg.beta * std::pow(cfg.c, 1 - cfg.p));
-
-    if (a > 0) {
-        std::size_t nc = 0;
-        auto start = std::chrono::high_resolution_clock::now();
-
-        if (verbose) {
-            std::cout << "Generating aftershocks...\n";
-            update_bar(seq[nc].t / cfg.tmax, m_max);
-        }
-
-        while (true) {
-            tc = 0;
-
-            if (verbose) {
-                const auto end = std::chrono::high_resolution_clock::now();
-                const std::chrono::duration<double> diff = end - start;
-
-                if (diff.count() > pbar_settings::delay) {
-                    update_bar(seq[nc].t / cfg.tmax, m_max);
-                    start = end;
-                }
-            }
-
-            while (true) {
-                const double tmp = std::pow(tc + cfg.c, 1 - cfg.p)
-                    + (cfg.p - 1) / (a * std::exp(cfg.alpha * seq[nc].m))
-                    * rd::logrand();
-
-                if (tmp > 0) {
-                    const double dt = std::pow(tmp, 1 / (1 - cfg.p))
-                        - tc - cfg.c;
-                    tc += dt;
-                    const double tc_nc = tc + seq[nc].t;
-
-                    if (tc_nc < cfg.tmax) {
-                        const double m = rd::mag(cfg.beta);
-
-                        m_max = std::max(m_max, m);
-                        seq.push_back({ tc_nc, m, nc + 1 });
-                    }
-                    else {
-                        break;
-                    }
-                }
-                else {
-                    break;
-                }
-            }
-
-            auto sort = [](const Point& p1, const Point& p2)
-            {
-                return p1.t < p2.t;
-            };
-            std::sort(std::execution::par, seq.begin(), seq.end(), sort);
-
-            ++nc;
-            if (nc >= seq.size())
-                break;
-
-            if (static_cast<int>(seq.size()) > cfg.max_len
-                    && cfg.generate_seqs)
-                return seq;
-        }
-    }
-
-    if (verbose)
-        close_bar(m_max);
-
-    return seq;
-}
-
-void write_to_file(const Sequence& seq,
-                   const std::string& filename,
-                   bool verbose = true)
-{
-    std::ofstream file{ filename };
-    std::size_t id = 0;
-
-    file << "ID,TIME,MAG,PARENT\n";
-    for (const Point& e : seq) {
-        file << ++id << ',' << e.t << ',' << e.m << ',' << e.parent << '\n';
-    }
-
-    if (verbose)
-        std::cout << seq.size() << " events written to file `"
-                  << filename << "`.\n";
-}
-
-void generate_seqs(const Config& cfg)
-{
-    if (!std::filesystem::create_directory(cfg.dirname)) {
-        std::cout << "Could not create directory, exiting...\n";
-        return;
-    }
-
-    if (cfg.verbose)
-        std::cout << "Generating sequences...\n";
-
-    for (int i = 1; i <= cfg.num_seqs; ++i) {
-        do {
-            const Sequence seq = etas(cfg);
-
-            if (static_cast<int>(seq.size()) <= cfg.max_len) {
-                const std::string filename = cfg.dirname + '/' + cfg.filename
-                    + std::to_string(i) + ".csv";
-
-                write_to_file(seq, filename, false);
-                break;
-            }
-        } while (true);
-
-        if (cfg.verbose)
-            update_bar(static_cast<double>(i) / cfg.num_seqs, "");
-    }
-
-    if (cfg.verbose)
-        close_bar("");
-}
-
-void print_args(const Config& cfg)
-{
-    std::cout << "Program configuration:\n";
-    std::cout << std::boolalpha;
-
-    std::cout << "  --tmax\t\t" << cfg.tmax << '\n';
-    std::cout << "  --mu\t\t\t" << cfg.mu << '\n';
-    std::cout << "  --alpha\t\t" << cfg.alpha << '\n';
-    std::cout << "  --bar_n\t\t" << cfg.bar_n << '\n';
-    std::cout << "  --p\t\t\t" << cfg.p << '\n';
-    std::cout << "  --c\t\t\t" << cfg.c << '\n';
-    std::cout << "  --beta\t\t" << cfg.beta << "\n\n";
-
-    std::cout << "  --generate_seqs\t" << cfg.generate_seqs << '\n';
-    std::cout << "  --num_seqs\t\t" << cfg.num_seqs << '\n';
-    std::cout << "  --max_len\t\t" << cfg.max_len << "\n\n";
-
-    std::cout << "  --filename\t\t" << cfg.filename << '\n';
-    std::cout << "  --dirname\t\t" << cfg.dirname << "\n\n";
-
-    std::cout << "  --verbose\t\t" << cfg.verbose << '\n';
-}
-
+// parsing command line arguments with argparse from p-ranav
+// ---------------------------------------------------------
 Config parse_arguments(int argc, char* argv[])
 {
     argparse::ArgumentParser program{ "ETAS" };
@@ -289,7 +87,7 @@ Config parse_arguments(int argc, char* argv[])
         .scan<'i', int>();
 
     program.add_argument("--filename")
-        .default_value(std::string{ "data.csv" });
+        .default_value(std::string{ "data" });
     program.add_argument("--dirname").default_value(std::string{ "data" });
 
     program.add_argument("--verbose").default_value(false)
@@ -332,14 +130,264 @@ Config parse_arguments(int argc, char* argv[])
     return cfg;
 }
 
+// print arguments if print_config is passed
+// -----------------------------------------
+void print_args(const Config& cfg)
+{
+    std::cout << "Program configuration:\n";
+    std::cout << std::boolalpha;
+
+    std::cout << "  --tmax\t\t" << cfg.tmax << '\n';
+    std::cout << "  --mu\t\t\t" << cfg.mu << '\n';
+    std::cout << "  --alpha\t\t" << cfg.alpha << '\n';
+    std::cout << "  --bar_n\t\t" << cfg.bar_n << '\n';
+    std::cout << "  --p\t\t\t" << cfg.p << '\n';
+    std::cout << "  --c\t\t\t" << cfg.c << '\n';
+    std::cout << "  --beta\t\t" << cfg.beta << "\n\n";
+
+    std::cout << "  --generate_seqs\t" << cfg.generate_seqs << '\n';
+    std::cout << "  --num_seqs\t\t" << cfg.num_seqs << '\n';
+    std::cout << "  --max_len\t\t" << cfg.max_len << "\n\n";
+
+    std::cout << "  --filename\t\t" << cfg.filename << '\n';
+    std::cout << "  --dirname\t\t" << cfg.dirname << "\n\n";
+
+    std::cout << "  --verbose\t\t" << cfg.verbose << '\n';
+}
+
+// progress bar
+// ------------------------------------------------------------------------------------------------
+
+constexpr double refresh_delay_in_s = 0.5;
+
+auto get_progress_bar(const std::string_view& prefix_text) -> indicators::ProgressBar
+{
+    using namespace indicators;
+
+    return ProgressBar{
+        option::BarWidth{40},
+        option::Start{"["},
+        option::Fill{"="},
+        option::Lead{">"},
+        option::Remainder{" "},
+        option::End{"]"},
+        option::ForegroundColor{Color::white},
+        option::ShowElapsedTime{true},
+        option::PrefixText{prefix_text},
+        option::FontStyles{std::vector<FontStyle>{FontStyle::bold}}
+    };
+}
+
+// simulation
+// ------------------------------------------------------------------------------------------------
+
+// random functions required for the simulation
+// --------------------------------------------
+namespace rd
+{
+    inline std::mt19937 init()
+    {
+        std::random_device rd;
+        std::seed_seq ss{ rd(), rd(), rd(), rd() };
+
+        return std::mt19937{ ss };
+    }
+
+    inline std::mt19937 mt = init();
+    inline std::uniform_real_distribution<double> d{ 0, 1 };
+
+    inline double logrand()
+    {
+        return std::log(d(mt));
+    }
+
+    inline double mag(double beta)
+    {
+        return -1 / beta * logrand();
+    }
+}
+
+struct Event
+{
+    double          t;
+    double          m;
+    std::size_t     parent;
+};
+
+// main logic for the ETAS algorithm
+// ---------------------------------
+Sequence etas(const Config& cfg)
+{
+    using namespace indicators;
+
+    Sequence seq;
+    double tc = 0;
+    double m_max = 0;
+    bool verbose = cfg.verbose && !cfg.generate_seqs;
+    auto progress_bar = get_progress_bar("Generating aftershocks...");
+
+    if (verbose)
+        std::cout << "Generating background earthquakes... \n";
+
+    while (tc < cfg.tmax) {
+        const double dt = -1 / cfg.mu * rd::logrand();
+
+        tc += dt;
+        if (tc < cfg.tmax) {
+            const double m = rd::mag(cfg.beta);
+
+            m_max = std::max(m_max, m);
+            seq.push_back({ tc, m, 0 });
+        }
+    }
+
+    const double a = cfg.bar_n * (cfg.p - 1) * (cfg.beta - cfg.alpha)
+        / (cfg.beta * std::pow(cfg.c, 1 - cfg.p));
+
+    if (verbose) {
+        progress_bar.set_progress(0);
+        progress_bar.set_option(option::PostfixText{ std::to_string(m_max) });
+    }
+
+    if (a > 0) {
+        std::size_t nc = 0;
+        auto start = std::chrono::high_resolution_clock::now();
+
+        while (true) {
+            tc = 0;
+
+            if (verbose) {
+                const auto end = std::chrono::high_resolution_clock::now();
+                const std::chrono::duration<double> diff = end - start;
+
+                if (diff.count() > refresh_delay_in_s) {
+                    progress_bar.set_progress(seq[nc].t / cfg.tmax * 100.0);
+                    progress_bar.set_option(option::PostfixText{ std::to_string(m_max) });
+                    start = end;
+                }
+            }
+
+            while (true) {
+                const double tmp = std::pow(tc + cfg.c, 1 - cfg.p)
+                    + (cfg.p - 1) / (a * std::exp(cfg.alpha * seq[nc].m))
+                    * rd::logrand();
+
+                if (tmp > 0) {
+                    const double dt = std::pow(tmp, 1 / (1 - cfg.p))
+                        - tc - cfg.c;
+                    tc += dt;
+                    const double tc_nc = tc + seq[nc].t;
+
+                    if (tc_nc < cfg.tmax) {
+                        const double m = rd::mag(cfg.beta);
+
+                        m_max = std::max(m_max, m);
+                        seq.push_back({ tc_nc, m, nc + 1 });
+                    }
+                    else {
+                        break;
+                    }
+                }
+                else {
+                    break;
+                }
+            }
+
+            auto sort = [](const Event& p1, const Event& p2)
+            {
+                return p1.t < p2.t;
+            };
+            std::sort(std::execution::par, seq.begin(), seq.end(), sort);
+
+            ++nc;
+            if (nc >= seq.size())
+                break;
+
+            if (static_cast<int>(seq.size()) > cfg.max_len
+                    && cfg.generate_seqs)
+                return seq;
+        }
+    }
+
+    if (verbose)
+        progress_bar.set_progress(100);
+
+    return seq;
+}
+
+// generate sequences and write each to its own file
+// -------------------------------------------------
+void generate_seqs(const Config& cfg)
+{
+    if (!std::filesystem::create_directory(cfg.dirname)) {
+        std::cout << "Could not create directory, exiting...\n";
+        return;
+    }
+
+    auto progress_bar = get_progress_bar("Generating sequences... ");
+    progress_bar.set_progress(0);
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 1; i <= cfg.num_seqs; ++i) {
+        do {
+            const Sequence seq = etas(cfg);
+
+            if (static_cast<int>(seq.size()) <= cfg.max_len) {
+                const std::string filename = cfg.dirname + '/' + cfg.filename
+                    + std::to_string(i);
+
+                write_to_file(seq, filename, false);
+                break;
+            }
+        } while (true);
+
+        const auto end = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<double> diff = end - start;
+
+        if (cfg.verbose) {
+            if (diff.count() > refresh_delay_in_s) {
+                progress_bar.set_progress(static_cast<double>(i) / cfg.num_seqs * 100.0);
+                start = end;
+            }
+        }
+    }
+
+    progress_bar.set_progress(100);
+}
+
+// take a sequence, a filename and write that sequence to the specified file with a CSV format
+// -------------------------------------------------------------------------------------------
+void write_to_file(const Sequence& seq,
+    const std::string& filename,
+    bool verbose = true)
+{
+    std::ofstream file{ filename + ".csv" };
+    std::size_t id = 0;
+
+    file << "ID,TIME,MAG,PARENT\n";
+    for (const Event& e : seq) {
+        file << ++id << ',' << e.t << ',' << e.m << ',' << e.parent << '\n';
+    }
+
+    if (verbose)
+        std::cout << seq.size() << " events written to file `"
+        << filename << ".csv`.\n";
+}
+
+// ------------------------------------------------------------------------------------------------
+
 int main(int argc, char* argv[])
 {
     const Config cfg = parse_arguments(argc, argv);
+
+    indicators::show_console_cursor(false);
 
     if (cfg.generate_seqs)
         generate_seqs(cfg);
     else
         write_to_file(etas(cfg), cfg.filename, cfg.verbose);
 
+    indicators::show_console_cursor(true);
     return 0;
 }
